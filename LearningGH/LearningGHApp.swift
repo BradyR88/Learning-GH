b273b38bed0d76e6138f72a09af3d37e99582237
Brady Robshaw <bradyr8@vt.edu>
2021-12-26 12:36:48 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Brady Robshaw on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
